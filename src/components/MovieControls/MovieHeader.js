83dbc28baab39cd7e82163c8d37c571ba90e3a29
johnallswell <johnsannithomas@gmail.com>
2022-10-21 02:52:24 -0400

    added an element

--- src/components/MovieControls/MovieHeader.js
+++ src/components/MovieControls/MovieHeader.js
@@ -3,6 +3,7 @@ import '../../styles/movie.css';
 
 const MovieHeader = (props) =>{
     return <div className='movie-header'>
+        <div className='postfix'>{'Name'}</div>
         <div className='caption'>{props.caption}</div>
         <div className='postfix'>{props.pref}</div>
     </div>
